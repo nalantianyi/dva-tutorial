e05cd5f9cb52134c4f73acb5c5ed360db1f0c1a3
纳兰天忆 <103520146@qq.com>
2016-12-26 21:04:02 +0800

    添加模拟数据

--- src/routes/Users.js
+++ src/routes/Users.js
@@ -1,7 +1,7 @@
 /**
  * Created by nalantianyi on 2016/12/22.
  */
-import React, {Component,PropTypes} from 'react';
+import React, {Component, PropTypes} from 'react';
 
 import UserList from '../components/Users/UserList';
 import UserModal from '../components/Users/UserModal';
@@ -13,7 +13,15 @@ import styles from '../Users.less';
 
 function Users() {
     const userSearchProps = {};
-    const userListProps = {};
+    const userListProps = {
+        total: 3,
+        current: 1,
+        loading: false,
+        dataSource: [
+            {name: '张三', age: 23, address: '南京'},
+            {name: '李四', age: 24, address: '杭州'},
+            {name: '小鸡', age: 27, address: '南京'}]
+    };
     const userModalProps = {};
     return (
         <div className={styles.normal}>
